9e8392c5c684979c2e7e4d8ed633484b83dda7de
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2022-01-15 17:57:46 +0900

    feat: recommend mix add

--- Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/RecommendTab/RecommendTabFragment.java
+++ Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/RecommendTab/RecommendTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.cocktail_pick.RecommendTab;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,15 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.cocktail_pick.Data.Cocktail;
+import com.example.cocktail_pick.Data.Recipe;
+import com.example.cocktail_pick.HomeTab.SummaryAdapter;
 import com.example.cocktail_pick.R;
 
 import java.util.ArrayList;
@@ -20,8 +26,13 @@ public class RecommendTabFragment extends Fragment {
     ViewPager2 viewPager;
     ArrayList<Cocktail> cocktails;
     RecommendViewPagerAdapter buying_adapter;
+    RecyclerView recommend_recycler_view;
+    SummaryAdapter summary_adapter;
     Button addBtn, removeBtn;
 
+    ArrayList<Recipe> recommendRecipes;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,9 +41,15 @@ public class RecommendTabFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_recommend, container, false);
 
         initCocktails();
+        initRecommendRecipes();
         viewPager = rootView.findViewById(R.id.buy_view_pager);
         buying_adapter = new RecommendViewPagerAdapter(getActivity(), cocktails);
         viewPager.setAdapter(buying_adapter);
+
+        recommend_recycler_view = rootView.findViewById(R.id.recommend_recycler_view);
+        recommend_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+        summary_adapter = new SummaryAdapter(getActivity(), recommendRecipes);
+        recommend_recycler_view.setAdapter(summary_adapter);
 
         addBtn = rootView.findViewById(R.id.add_buying_btn);
         removeBtn = rootView.findViewById(R.id.remove_buying_btn);
@@ -61,6 +78,14 @@ public class RecommendTabFragment extends Fragment {
 
         for (int i = 0; i < 10; i++) {
             cocktails.add(new Cocktail("잭 다니엘", ""));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void initRecommendRecipes() {
+        recommendRecipes = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            recommendRecipes.add(new Recipe());
         }
     }
 }
