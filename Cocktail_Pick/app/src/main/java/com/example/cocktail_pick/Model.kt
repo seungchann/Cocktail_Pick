c8ba91828fa27f5d670c511f6bd1cdcf2d43dabd
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2022-01-16 18:00:01 +0900

    fix: Member data class

--- Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/Model.kt
+++ Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/Model.kt
@@ -4,9 +4,15 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Member (
-    @SerializedName("nick_name")
-    var nickName: String,
+    var id: Int,
     var email: String,
-    @SerializedName("profile_image_url")
-    var profileImageURL: String
+    @SerializedName("username")
+    var userName: String,
+    @SerializedName("profile_url")
+    var profileURL: String,
+//    @SerializedName("prefer_recipe_list")
+//    var preferRecipeList:
+    @SerializedName("my_tag")
+    var myTag: List<String>,
 ): Serializable
+
