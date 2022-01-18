63abb49acfc2b2fe921ee05ed872471b74d5b70b
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2022-01-18 15:25:12 +0900

    feat: add recipe select tag

--- Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/HomeTab/DetailRecipe/SpinnerAdapter.java
+++ Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/HomeTab/DetailRecipe/SpinnerAdapter.java
@@ -1,32 +1,54 @@
 package com.example.cocktail_pick.HomeTab.DetailRecipe;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.cocktail_pick.Data.Base;
+import com.example.cocktail_pick.R;
 
 import java.util.ArrayList;
 
 public class SpinnerAdapter extends BaseAdapter {
-    Context context;
-    ArrayList<String >
+
+    Base[] bases;
+    LayoutInflater inflater;
+    public SpinnerAdapter(Context context) {
+        bases = Base.values();
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return bases.length;
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return bases[i].toString();
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_spinner, viewGroup, false);
+        }
+
+        if (bases != null) {
+            String text = bases[i].toString();
+            ((TextView) view.findViewById(R.id.spinner_item_tv)).setText(text);
+        }
+        return view;
     }
 }
