eda37b94f0d09cfafd1de3712360dadbacf651bd
SChan-Kim <instufld@gmail.com>
2022-01-18 16:46:06 +0900

    feat: Tag 별로 칵테일 레시피 뜨도록

--- Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/HomeTab/HomeTabFragment.java
+++ Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/HomeTab/HomeTabFragment.java
@@ -16,7 +16,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,12 +25,14 @@ import com.example.cocktail_pick.Main.MainViewModelFactory;
 import com.example.cocktail_pick.MainRepository;
 import com.example.cocktail_pick.Member;
 import com.example.cocktail_pick.R;
-import com.example.cocktail_pick.Recipe;
 import com.example.cocktail_pick.RecipeReceive;
 import com.example.cocktail_pick.RetrofitService;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
 
 public class HomeTabFragment extends Fragment {
     String TAG = "HomeTabFragment";
@@ -39,7 +40,7 @@ public class HomeTabFragment extends Fragment {
     ImageView profileImage;
     TextView profileName;
     SummaryAdapter summaryAdapter;
-    ArrayList<Recipe> recipes;
+    ArrayList<RecipeReceive> recipes;
     Button testbtn;
     MainViewModel viewModel;
     RetrofitService retrofitService = RetrofitService.Companion.getInstance();
@@ -68,7 +69,39 @@ public class HomeTabFragment extends Fragment {
         viewModel.getTagBasedRecipeList().observe(getViewLifecycleOwner(), new Observer<List<RecipeReceive>>() {
             @Override
             public void onChanged(List<RecipeReceive> recipeReceives) {
-                summaryAdapter = new SummaryAdapter(getActivity(), (ArrayList<RecipeReceive>) recipeReceives);
+                recipes = new ArrayList<RecipeReceive>();
+                viewModel.loadUserAccount();
+                viewModel.getCurrentUser().observe(getViewLifecycleOwner(), new Observer<List<Member>>() {
+                    @Override
+                    public void onChanged(List<Member> members) {
+                        members.get(0).getTag().forEach(new Consumer<Integer>() {
+                            @Override
+                            public void accept(Integer integer) {
+                                recipeReceives.forEach(new Consumer<RecipeReceive>() {
+                                    @Override
+                                    public void accept(RecipeReceive recipeReceive) {
+                                        List<Integer> tags = recipeReceive.getTags();
+                                        tags.forEach(new Consumer<Integer>() {
+                                            @Override
+                                            public void accept(Integer tagInteger) {
+                                                if(integer == tagInteger) {
+                                                    recipes.add(recipeReceive);
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+
+                    }
+                });
+
+                // 중복 제거
+                HashSet<RecipeReceive> distinctRecipes = new HashSet<>(recipes);
+                recipes = new ArrayList<>(distinctRecipes);
+
+                summaryAdapter = new SummaryAdapter(getActivity(), recipes);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
                 recyclerView.setAdapter(summaryAdapter);
             }
@@ -97,7 +130,7 @@ public class HomeTabFragment extends Fragment {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     void init_recipes() {
-        recipes = new ArrayList<>();
+        recipes = new ArrayList<RecipeReceive>();
 
         for (int i = 0; i < 10; i++) {
 //            recipes.add(new Recipe());
--- Cocktail_Pick/app/src/main/java/com/example/cocktail_pick/HomeTab/HomeTabViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cocktail_pick.HomeTab
-
-import androidx.lifecycle.ViewModel
-import com.example.cocktail_pick.MainRepository
-
-class HomeTabViewModel constructor(private val repository: MainRepository) : ViewModel() {
-}
